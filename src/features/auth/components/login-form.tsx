3ca01ffb8b18a488f5f3ddf2cd1d41de33fc0221
ahargunyllib <billy.bpm03@gmail.com>
2025-04-30 16:21:55 +0700

    feat: add login page and form components for user authentication

---
+++ src/features/auth/components/login-form.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@/shared/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/shared/components/ui/card";
+import { Input } from "@/shared/components/ui/input";
+import { Label } from "@/shared/components/ui/label";
+import { cn } from "@/shared/lib/utils";
+
+export function LoginForm({
+	className,
+	...props
+}: React.ComponentProps<"div">) {
+	return (
+		<div className={cn("flex flex-col gap-6", className)} {...props}>
+			<Card>
+				<CardHeader>
+					<CardTitle>Login to your account</CardTitle>
+					<CardDescription>
+						Enter your email below to login to your account
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<form>
+						<div className="flex flex-col gap-6">
+							<div className="grid gap-3">
+								<Label htmlFor="email">Email</Label>
+								<Input id="email" type="email" placeholder="m@example.com" />
+							</div>
+							<div className="grid gap-3">
+								<div className="flex items-center">
+									<Label htmlFor="password">Password</Label>
+								</div>
+								<Input id="password" type="password" />
+							</div>
+							<Button type="submit" className="w-full">
+								Login
+							</Button>
+						</div>
+					</form>
+				</CardContent>
+			</Card>
+		</div>
+	);
+}
