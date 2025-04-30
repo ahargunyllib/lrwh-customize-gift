a044092eb8eec762bd4bc2566183d6cb6043d139
ahargunyllib <billy.bpm03@gmail.com>
2025-04-30 18:50:41 +0700

    feat: add token encoding function with environment variable support

--- src/shared/lib/decode.ts
+++ src/shared/lib/decode.ts
@@ -1,4 +1,5 @@
 import * as jose from "jose";
+import { env } from "../../env.mjs";
 import type { roleEnum } from "./enums";
 
 interface TokenPayload {
@@ -10,4 +11,16 @@ interface TokenPayload {
 
 export function decodeToken(token: string): TokenPayload {
 	return jose.decodeJwt(token);
+}
+
+export async function encodeToken(payload: jose.JWTPayload) {
+	const jwt = new jose.SignJWT(payload)
+		.setProtectedHeader({ alg: "HS256" })
+		.setExpirationTime(env.SESSION_EXPIRATION_TIME); // 1 day
+
+	const secret = new TextEncoder().encode(env.SESSION_SECRET);
+
+	const token = await jwt.sign(secret);
+
+	return token;
 }
