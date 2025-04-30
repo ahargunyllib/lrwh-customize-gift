3f62df8917e276241137a84dac2c5149231b0e21
ahargunyllib <billy.bpm03@gmail.com>
2025-04-30 16:53:50 +0700

    feat: add providers for theme and React Query management in layout

---
+++ src/shared/components/providers/index.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import type React from "react";
+
+import { Toaster } from "../ui/sonner";
+import ReactQueryProvider from "./react-query-provider";
+import { ThemeProvider } from "./theme-provider";
+
+export default function Provider({ children }: React.PropsWithChildren) {
+	return (
+		<ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+			<ReactQueryProvider>
+				{children}
+				<Toaster />
+			</ReactQueryProvider>
+		</ThemeProvider>
+	);
+}
