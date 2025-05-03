41637e03e8469fa9187ba7aab91d58cfbc8eeee7
rmvs <nofalrmz@gmail.com>
2025-05-03 23:31:29 +0700

    feat: implement orders table data fetch

--- src/app/dashboard/order-management/page.tsx
+++ src/app/dashboard/order-management/page.tsx
@@ -1,3 +1,33 @@
+"use client";
+
+import {
+	DataTable,
+	columns,
+} from "@/features/order-management/components/order-table";
+import { useGetOrdersQuery } from "@/shared/repository/order/query";
+import { useSearchParams } from "next/navigation";
+
 export default function Page() {
-	return <section>Order Management</section>;
+	return (
+		<section>
+			Order Management
+			<OrderContainer />
+		</section>
+	);
+}
+
+function OrderContainer() {
+	const sp = useSearchParams();
+
+	const { data: res, isLoading } = useGetOrdersQuery({
+		// search: sp.get("search") || "",
+	});
+
+	if (isLoading) return <div>Loading...</div>;
+
+	if (!res?.data) return <div>No data</div>;
+
+	const orders = res.data.orders;
+
+	return <DataTable columns={columns} data={orders} />;
 }
