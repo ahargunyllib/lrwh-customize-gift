8689bb5150cba392d437cc6848788e750d5eb6c6
ahargunyllib <billy.bpm03@gmail.com>
2025-05-03 20:48:01 +0700

    feat: update orders table schema to use camelCase for field names

--- src/server/db/schema/orders.ts
+++ src/server/db/schema/orders.ts
@@ -2,8 +2,8 @@ import { integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const ordersTable = pgTable("orders", {
 	id: integer().primaryKey().generatedAlwaysAsIdentity(),
-	order_number: varchar({ length: 255 }).notNull(),
+	orderNumber: varchar({ length: 255 }).notNull(),
 	username: varchar({ length: 255 }).notNull(),
-	image_url: varchar({ length: 255 }).notNull(),
+	imageUrl: varchar({ length: 255 }).notNull(),
 	createdAt: timestamp("created_at").defaultNow().notNull(),
 });
