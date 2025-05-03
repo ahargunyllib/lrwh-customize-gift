74d88fabf685fd97d81e8c12c7f9f44749dd8bfe
ahargunyllib <billy.bpm03@gmail.com>
2025-05-03 21:20:21 +0700

    feat: make imageUrl field optional in orders table schema

--- src/server/db/schema/orders.ts
+++ src/server/db/schema/orders.ts
@@ -4,6 +4,6 @@ export const ordersTable = pgTable("orders", {
 	id: integer().primaryKey().generatedAlwaysAsIdentity(),
 	orderNumber: varchar({ length: 255 }).notNull(),
 	username: varchar({ length: 255 }).notNull(),
-	imageUrl: varchar({ length: 255 }).notNull(),
+	imageUrl: varchar({ length: 255 }),
 	createdAt: timestamp("created_at").defaultNow().notNull(),
 });
