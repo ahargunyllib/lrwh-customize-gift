ae0791bf5f4f9b2cc286496144904b69b7ef51d3
ahargunyllib <billy.bpm03@gmail.com>
2025-05-04 15:13:30 +0700

    feat: implement CRUD operations for orders with validation and state management

--- src/shared/repository/order/action.ts
+++ src/shared/repository/order/action.ts
@@ -1,20 +1,28 @@
 "use server";
 
 import { db } from "@/server/db";
+import { ordersTable } from "@/server/db/schema/orders";
+import { eq, sql } from "drizzle-orm";
 import { tryCatch } from "../../lib/try-catch";
 import type { Order } from "../../types";
-import type { GetOrdersQuery } from "./dto";
+import type {
+	CreateOrderRequest,
+	GetOrdersQuery,
+	UpdateOrderParams,
+	UpdateOrderRequest,
+} from "./dto";
 
 export const getOrders = async (query: GetOrdersQuery) => {
-	let queryBuilder = `
+	const queryBuilder = sql`
     select *
     from orders
   `;
-	const args = [];
 
-	if (query?.search) {
-		queryBuilder += ` where order_number ilike $${args.length + 1} or username ilike $${args.length + 1}`;
-		args.push(`%${query.search}%`);
+	if (query?.search && query.search.length > 0) {
+		query.search = `%${query.search}%`;
+		queryBuilder.append(
+			sql` where "orderNumber" ilike ${query.search} or username ilike ${query.search}`,
+		);
 	}
 
 	if (query?.page && query?.limit) {
@@ -27,12 +35,12 @@ export const getOrders = async (query: GetOrdersQuery) => {
 		}
 
 		const offset = (query.page - 1) * query.limit;
-		queryBuilder += ` limit $${args.length + 1} offset $${args.length + 2}`;
-		args.push(query.limit, offset);
+		queryBuilder.append(sql` limit ${query.limit} offset ${offset}`);
 	}
 
 	const { data, error } = await tryCatch(db.execute<Order>(queryBuilder));
 	if (error) {
+		console.error(error);
 		return {
 			success: false,
 			error: error.message,
@@ -48,5 +56,80 @@ export const getOrders = async (query: GetOrdersQuery) => {
 			orders,
 		},
 		message: "Orders fetched successfully",
+	};
+};
+
+export const createOrder = async ({
+	orderNumber,
+	username,
+}: CreateOrderRequest) => {
+	const { error } = await tryCatch(
+		db.insert(ordersTable).values({
+			orderNumber,
+			username,
+		}),
+	);
+	if (error) {
+		console.error(error);
+		return {
+			success: false,
+			error: error.message,
+			message: "Failed to create order",
+		};
+	}
+
+	return {
+		success: true,
+		data: null,
+		message: "Order created successfully",
+	};
+};
+
+export const updateOrder = async (
+	{ id }: UpdateOrderParams,
+	{ orderNumber, username }: UpdateOrderRequest,
+) => {
+	const { error } = await tryCatch(
+		db
+			.update(ordersTable)
+			.set({
+				orderNumber,
+				username,
+			})
+			.where(eq(ordersTable.id, id)),
+	);
+	if (error) {
+		console.error(error);
+		return {
+			success: false,
+			error: error.message,
+			message: "Failed to update order",
+		};
+	}
+
+	return {
+		success: true,
+		data: null,
+		message: "Order updated successfully",
+	};
+};
+
+export const deleteOrder = async ({ id }: UpdateOrderParams) => {
+	const { error } = await tryCatch(
+		db.delete(ordersTable).where(eq(ordersTable.id, id)),
+	);
+	if (error) {
+		console.error(error);
+		return {
+			success: false,
+			error: error.message,
+			message: "Failed to delete order",
+		};
+	}
+
+	return {
+		success: true,
+		data: null,
+		message: "Order deleted successfully",
 	};
 };
--- src/shared/repository/order/query.ts
+++ src/shared/repository/order/query.ts
@@ -1,12 +1,82 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
-import { getOrders } from "./action";
-import type { GetOrdersQuery } from "./dto";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { useDialogStore } from "../../hooks/use-dialog";
+import { createOrder, deleteOrder, getOrders, updateOrder } from "./action";
+import type {
+	CreateOrderRequest,
+	GetOrdersQuery,
+	UpdateOrderParams,
+	UpdateOrderRequest,
+} from "./dto";
 
 export const useGetOrdersQuery = (query: GetOrdersQuery) => {
 	return useQuery({
 		queryKey: ["orders", query],
 		queryFn: () => getOrders(query),
+	});
+};
+
+export const useCreateOrderMutation = () => {
+	const queryClient = useQueryClient();
+	const { closeDialog } = useDialogStore();
+
+	return useMutation({
+		mutationKey: ["orders"],
+		mutationFn: (data: CreateOrderRequest) => createOrder(data),
+		onSuccess: (res) => {
+			if (!res.success) {
+				toast.error(res.message);
+				return;
+			}
+
+			toast.success(res.message);
+
+			closeDialog();
+			queryClient.invalidateQueries({
+				queryKey: ["orders"],
+			});
+		},
+	});
+};
+
+export const useUpdateOrderMutation = (id: UpdateOrderParams) => {
+	const queryClient = useQueryClient();
+
+	return useMutation({
+		mutationKey: ["orders"],
+		mutationFn: (data: UpdateOrderRequest) => updateOrder(id, data),
+		onSuccess: (res) => {
+			if (!res.success) {
+				toast.error(res.message);
+				return;
+			}
+
+			toast.success(res.message);
+			queryClient.invalidateQueries({
+				queryKey: ["orders"],
+			});
+		},
+	});
+};
+
+export const useDeleteOrderMutation = (id: UpdateOrderParams) => {
+	const queryClient = useQueryClient();
+
+	return useMutation({
+		mutationKey: ["orders"],
+		mutationFn: () => deleteOrder(id),
+		onSuccess: (res) => {
+			if (!res.success) {
+				toast.error(res.message);
+				return;
+			}
+
+			toast.success(res.message);
+			queryClient.invalidateQueries({
+				queryKey: ["orders"],
+			});
+		},
 	});
 };
