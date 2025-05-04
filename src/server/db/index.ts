ac0df6e7e7fcc6779f35143303ea7032582c371e
ahargunyllib <billy.bpm03@gmail.com>
2025-05-04 15:12:38 +0700

    feat: enable logging for database connection

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -3,4 +3,4 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { env } from "../../env.mjs";
 
 const sql = neon(env.DATABASE_URL);
-export const db = drizzle({ client: sql });
+export const db = drizzle({ client: sql, logger: true });
