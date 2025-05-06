9bfb7a55646205fb7a8d252b48995416629f211f
kenziehh <ghaisani.nurani@gmail.com>
2025-05-06 17:39:13 +0700

    `feat` : add letter spacing text style

--- src/shared/types/template.ts
+++ src/shared/types/template.ts
@@ -1,65 +1,66 @@
 export interface Position {
-  x: number;
-  y: number;
+	x: number;
+	y: number;
 }
 
 export interface ImageElement {
-  id: string;
-  type: "image";
-  src: string;
-  position: Position;
-  width: number;
-  height: number;
+	id: string;
+	type: "image";
+	src: string;
+	position: Position;
+	width: number;
+	height: number;
 }
 
 export interface TextElement {
-  id: string;
-  type: "text";
-  content: string;
-  position: Position;
-  style: {
-    fontFamily: string;
-    fontSize: string;
-    fontWeight: string;
-    color: string;
-    textAlign: string;
-    lineHeight: string;
-    curved?: boolean;
-    curveRadius?: number;
-    curveDirection?: "up" | "down";
-    rotate?: number;
-    centerX?: boolean;
-    maxWidth?: number | string;
-    backgroundColor?: string;
-    borderRadius?: number;
-    height?: number;
-    padding?: string | number;
-    paddingTop?: string | number;
-    paddingRight?: string | number;
-    paddingBottom?: string | number;
-    paddingLeft?: string | number;
-    paddingX?: string | number;
-    paddingY?: string | number;
-    paddingCenter?: boolean;
-  };
+	id: string;
+	type: "text";
+	content: string;
+	position: Position;
+	style: {
+		fontFamily: string;
+		fontSize: string;
+		fontWeight: string;
+		color: string;
+		textAlign: string;
+		lineHeight: string;
+		curved?: boolean;
+		curveRadius?: number;
+		curveDirection?: "up" | "down";
+		rotate?: number;
+		centerX?: boolean;
+		maxWidth?: number | string;
+		backgroundColor?: string;
+		borderRadius?: number;
+		height?: number;
+		padding?: string | number;
+		paddingTop?: string | number;
+		paddingRight?: string | number;
+		paddingBottom?: string | number;
+		paddingLeft?: string | number;
+		paddingX?: string | number;
+		paddingY?: string | number;
+		paddingCenter?: boolean;
+		letterSpacing?: string | number;
+	};
 }
 
 export interface TemplateData {
-  id: string;
-  name: string;
-  width: number;
-  height: number;
-  backgroundColor: string;
-  backgroundImage?: string;
-  images: ImageElement[];
-  texts: TextElement[];
+	id: string;
+	name: string;
+	width: number;
+	height: number;
+	backgroundColor: string;
+	backgroundImage?: string;
+	images: ImageElement[];
+	texts: TextElement[];
 }
 
 export type PrintSize = "10x20" | "15x20" | "20x30";
 
 export interface PrintSizeConfig {
-  name: PrintSize;
-  width: number;
-  height: number;
-  label: string;
+	name: PrintSize;
+	width: number;
+	height: number;
+	label: string;
 }
