4a9cab4e84a9de86456c8d51523f63b4a4b48078
kenziehh <ghaisani.nurani@gmail.com>
2025-05-06 17:23:15 +0700

    `feat`: add custom fonts Flatlion, More Sugar, and Mungil with @font-face declarations

---
+++ src/shared/lib/font.ts
@@ -0,0 +1,7 @@
+export const fontFamily = {
+	flatlion: "'Flatlion', sans-serif",
+	moreSugar: "'More Sugar', cursive",
+	mungil: "'Mungil', sans-serif",
+};
+
+export type FontType = typeof fontFamily;
