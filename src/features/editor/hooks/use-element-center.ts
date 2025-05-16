928ebf9374edb49c40422a56e162eeb71e514e76
kenziehh <ghaisani.nurani@gmail.com>
2025-05-16 17:44:36 +0700

    `fix`: fix element center hooks functionallity

--- src/features/editor/hooks/use-element-center.ts
+++ src/features/editor/hooks/use-element-center.ts
@@ -1,4 +1,4 @@
-"use client";
+import type { Axis } from "@/shared/lib/events";
 import type { TemplateData } from "@/shared/types/template";
 import { useEffect } from "react";
 
@@ -6,52 +6,40 @@ export function useElementCenter(
 	setTemplate: React.Dispatch<React.SetStateAction<TemplateData>>,
 ) {
 	useEffect(() => {
-		const handler = (e: Event) => {
-			const { id, type, axis } = (
-				e as CustomEvent<{
-					id: string;
-					type: string;
-					axis: "x" | "y" | "both";
-				}>
-			).detail;
+		function handler(e: Event) {
+			const { id, axis } = (e as CustomEvent<{ id: string; axis: Axis }>)
+				.detail;
 
-			setTemplate((prev) => {
-				if (type === "image") {
-					return {
-						...prev,
-						images: prev.images.map((img) =>
-							img.id === id
-								? {
-										...img,
-										centerX: axis === "x" || axis === "both" || img.centerX,
-										centerY: axis === "y" || axis === "both" || img.centerY,
-									}
-								: img,
-						),
-					};
-				}
-				if (type === "text") {
-					return {
-						...prev,
-						texts: prev.texts.map((txt) =>
-							txt.id === id
-								? {
-										...txt,
-										style: {
-											...txt.style,
-											centerX:
-												axis === "x" || axis === "both" || txt.style.centerX,
-											centerY:
-												axis === "y" || axis === "both" || txt.style.centerY,
-										},
-									}
-								: txt,
-						),
-					};
-				}
-				return prev;
-			});
-		};
+			setTemplate((prev) => ({
+				...prev,
+
+				images: prev.images.map((img) =>
+					img.id === id
+						? {
+								...img,
+								centerX: axis === "x" || axis === "both" ? true : img.centerX,
+								centerY: axis === "y" || axis === "both" ? true : img.centerY,
+							}
+						: img,
+				),
+
+				texts: prev.texts.map((txt) =>
+					txt.id === id
+						? {
+								...txt,
+								style: {
+									...txt.style,
+									centerX:
+										axis === "x" || axis === "both" ? true : txt.style.centerX,
+									centerY:
+										axis === "y" || axis === "both" ? true : txt.style.centerY,
+								},
+							}
+						: txt,
+				),
+			}));
+		}
+
 		document.addEventListener("elementCenter", handler);
 		return () => document.removeEventListener("elementCenter", handler);
 	}, [setTemplate]);
