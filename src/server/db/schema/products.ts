556599214e46524a720462d1fd798cfe76291783
ahargunyllib <billy.bpm03@gmail.com>
2025-07-06 11:42:33 +0700

    feat: add products and product variants tables to database schema

---
+++ src/server/db/schema/products.ts
@@ -0,0 +1,27 @@
+import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const productsTable = pgTable("products", {
+	id: uuid("id").primaryKey().defaultRandom(),
+	name: varchar("name", { length: 255 }).notNull(),
+	description: varchar("description", { length: 1000 }),
+	shopeeUrl: varchar("shopee_url", { length: 255 }),
+	createdAt: timestamp("created_at").notNull().defaultNow(),
+	updatedAt: timestamp("updated_at")
+		.notNull()
+		.defaultNow()
+		.$onUpdate(() => new Date()),
+});
+
+export const productVariantsTable = pgTable("product_variants", {
+	id: uuid("id").primaryKey().defaultRandom(),
+	productId: uuid("product_id")
+		.notNull()
+		.references(() => productsTable.id, { onDelete: "cascade" }),
+	name: varchar("variant_name", { length: 255 }).notNull(),
+	description: varchar("description", { length: 1000 }),
+	createdAt: timestamp("created_at").notNull().defaultNow(),
+	updatedAt: timestamp("updated_at")
+		.notNull()
+		.defaultNow()
+		.$onUpdate(() => new Date()),
+});
