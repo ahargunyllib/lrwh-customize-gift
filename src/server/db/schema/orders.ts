26b7f7cac4affaf33bb04075e5dde9042b0d9043
ahargunyllib <billy.bpm03@gmail.com>
2025-07-13 10:44:14 +0700

    feat: change order ID type from integer to UUID with default random generation

--- src/server/db/schema/orders.ts
+++ src/server/db/schema/orders.ts
@@ -1,7 +1,7 @@
-import { integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const ordersTable = pgTable("orders", {
-	id: integer().primaryKey().generatedAlwaysAsIdentity(),
+	id: uuid("id").primaryKey().defaultRandom(),
 	orderNumber: varchar("order_number", { length: 255 }).notNull(),
 	username: varchar({ length: 255 }).notNull(),
 	imageUrl: varchar("image_url", { length: 255 }),
