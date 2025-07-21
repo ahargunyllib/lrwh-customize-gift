3f2fd25a4b33ce8a19091755a30d1fbdaff4a573
ahargunyllib <billy.bpm03@gmail.com>
2025-07-21 11:33:07 +0700

    feat: add productVariantId to templates table with foreign key reference

--- src/server/db/schema/templates.ts
+++ src/server/db/schema/templates.ts
@@ -1,9 +1,20 @@
 import type { TemplateData } from "@/shared/types/template";
-import { json, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
+import {
+	json,
+	pgTable,
+	text,
+	timestamp,
+	uuid,
+	varchar,
+} from "drizzle-orm/pg-core";
+import { productVariantsTable } from "./products";
 
 export const templatesTable = pgTable("templates", {
 	id: text("id").primaryKey(),
 	name: varchar({ length: 255 }).notNull(),
+	productVariantId: uuid("product_variant_id")
+		.notNull()
+		.references(() => productVariantsTable.id, { onDelete: "cascade" }),
 	data: json("data").notNull().$type<Omit<TemplateData, "id" | "name">>(),
 	createdAt: timestamp("created_at").defaultNow().notNull(),
 });
