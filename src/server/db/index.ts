9ae2caaa230fe353a53173ed960d87256efc06d4
ahargunyllib <billy.bpm03@gmail.com>
2025-07-21 11:00:02 +0700

    feat: update database connection to use Pool for improved performance

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,6 +1,8 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import { Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
 import { env } from "../../env.mjs";
 
-const sql = neon(env.DATABASE_URL);
-export const db = drizzle({ client: sql });
+const pool = new Pool({
+	connectionString: env.DATABASE_URL,
+});
+export const db = drizzle({ client: pool });
