ad316a01d900559b3d8ed761977fa14764661727
ahargunyllib <billy.bpm03@gmail.com>
2025-08-04 16:15:26 +0700

    feat: add onboarding tips dialog to enhance user guidance in the onboarding form

--- src/features/onboarding/components/onboarding-form.tsx
+++ src/features/onboarding/components/onboarding-form.tsx
@@ -11,7 +11,18 @@ import {
 	FormMessage,
 } from "@/shared/components/ui/form";
 import { Input } from "@/shared/components/ui/input";
-import { InfoIcon, Loader } from "lucide-react";
+import { ArrowLeftIcon, InfoIcon, Loader } from "lucide-react";
+import Image from "next/image";
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "../../../shared/components/ui/dialog";
+import { useIsMobile } from "../../../shared/hooks/use-mobile";
+import { cn } from "../../../shared/lib/utils";
 import { useOnboardingForm } from "../hooks/use-onboarding-form";
 
 export default function OnboardingForm() {
@@ -33,7 +44,7 @@ export default function OnboardingForm() {
 									Order ID
 									<span className="text-red-500">*</span>
 								</FormLabel>
-								<InfoIcon className="text-[#2854AD] size-4" />
+								<OnboardingTipsDialog />
 							</div>
 							<FormControl>
 								<Input
@@ -92,5 +103,63 @@ export default function OnboardingForm() {
 				</Button>
 			</form>
 		</Form>
+	);
+}
+
+function OnboardingTipsDialog() {
+	const isMobile = useIsMobile();
+	const tab = isMobile ? "aplikasi" : "website";
+
+	return (
+		<Dialog>
+			<DialogTrigger asChild>
+				<InfoIcon className="text-[#2854AD] size-4" />
+			</DialogTrigger>
+			<DialogContent
+				showCloseButton={false}
+				className={cn(
+					tab === "website" && "sm:max-w-[39rem]",
+					tab === "aplikasi" && "sm:max-w-[27rem]",
+				)}
+			>
+				<DialogHeader className="text-left">
+					<DialogClose asChild>
+						<ArrowLeftIcon className="absolute top-6 left-4 size-6 text-[#292D32]" />
+					</DialogClose>
+					<DialogTitle className="text-center text-base font-bold text-[#1D2939]">
+						Cara cek order ID kamu
+					</DialogTitle>
+					<ul className="list-disc pl-4 text-[#737373] text-sm">
+						<li>Pilih dulu pesanan yang mau kamu lihat</li>
+						<li>
+							Terus scroll ke bawah sampai ketemu bagian "Rincian Pesanan".
+						</li>
+						<li>
+							Nah, di situ kamu bisa lihat detail, termasuk Nomor Pesanan atau
+							Order ID-nya
+						</li>
+					</ul>
+				</DialogHeader>
+				{isMobile ? (
+					<div className="relative bg-muted w-full h-[14rem]">
+						<Image
+							src="/mobile-tip.png"
+							alt="Mobile Tips"
+							fill
+							className="object-contain"
+						/>
+					</div>
+				) : (
+					<div className="relative bg-muted w-[36rem] h-[8rem]">
+						<Image
+							src="/website-tip.png"
+							alt="Website Tips"
+							fill
+							className="object-contain"
+						/>
+					</div>
+				)}
+			</DialogContent>
+		</Dialog>
 	);
 }
