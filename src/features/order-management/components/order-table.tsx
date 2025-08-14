cf3f8e6adb3b5d9d357e5e48a552ae33fc063147
ahargunyllib <billy.bpm03@gmail.com>
2025-08-14 15:29:41 +0700

    feat: add Add Order button and integrate AddOrderDialog in order management page

--- src/features/order-management/components/order-table.tsx
+++ src/features/order-management/components/order-table.tsx
@@ -9,8 +9,13 @@ import {
 	TableHeader,
 	TableRow,
 } from "@/shared/components/ui/table";
-import { useDialogStore } from "@/shared/hooks/use-dialog";
-import type { Order } from "@/shared/types";
+import { tryCatch } from "@/shared/lib/try-catch";
+import type {
+	Order,
+	OrderProductVariant,
+	Product,
+	ProductVariant,
+} from "@/shared/types";
 import {
 	type ColumnDef,
 	flexRender,
@@ -18,54 +23,119 @@ import {
 	getPaginationRowModel,
 	useReactTable,
 } from "@tanstack/react-table";
-import AddOrderDialogContainer from "../container/add-order-dialog-container";
-import { DataTablePagination } from "./order-table-pagination";
-import SearchFilter from "./search-filter";
+import { ChevronDownIcon, ChevronRightIcon, EyeIcon } from "lucide-react";
+import Image from "next/image";
+import { Fragment, useState } from "react";
+import { toast } from "sonner";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "../../../shared/components/ui/dialog";
 
-type OrderColumn = Order;
+type Props = {
+	data: {
+		id: Order["id"];
+		orderNumber: Order["orderNumber"];
+		username: Order["username"];
+		createdAt: Order["createdAt"];
+		products: {
+			id: Product["id"];
+			name: Product["name"];
+			productVariant: {
+				id: ProductVariant["id"];
+				name: ProductVariant["name"];
+				width: ProductVariant["width"];
+				height: ProductVariant["height"];
+			};
+			imageUrl: OrderProductVariant["imageUrl"];
+		}[];
+	}[];
+};
 
-export const columns: ColumnDef<OrderColumn>[] = [
-	{
-		accessorKey: "id",
-		header: "ID",
-	},
-	{
-		accessorKey: "orderNumber",
-		header: "Order Number",
-	},
-	{
-		accessorKey: "username",
-		header: "Username",
-	},
-	{
-		accessorKey: "createdAt",
-		header: "Created At",
-		cell(props) {
-			const date = new Date(props.getValue() as string);
-			return (
-				<span>
-					{date.toLocaleDateString("en-US", {
-						year: "numeric",
-						month: "short",
-						day: "2-digit",
-						hour: "2-digit",
-						minute: "2-digit",
-					})}
-				</span>
-			);
+export default function OrderTable({ data }: Props) {
+	const [expandedRows, setExpandedRows] = useState<Set<string>>(new Set());
+
+	const toggleRowExpansion = (rowId: string) => {
+		const newExpanded = new Set(expandedRows);
+		if (newExpanded.has(rowId)) {
+			newExpanded.delete(rowId);
+		} else {
+			newExpanded.add(rowId);
+		}
+		setExpandedRows(newExpanded);
+	};
+
+	const columns: ColumnDef<{
+		id: Order["id"];
+		orderNumber: Order["orderNumber"];
+		username: Order["username"];
+		createdAt: Order["createdAt"];
+		products: {
+			id: Product["id"];
+			name: Product["name"];
+			productVariant: {
+				id: ProductVariant["id"];
+				name: ProductVariant["name"];
+				width: ProductVariant["width"];
+				height: ProductVariant["height"];
+			};
+			imageUrl: OrderProductVariant["imageUrl"];
+		}[];
+	}>[] = [
+		{
+			id: "expand",
+			header: "",
+			cell: ({ row }) => (
+				<Button
+					variant="ghost"
+					size="sm"
+					onClick={() => toggleRowExpansion(row.original.id)}
+					className="p-0 h-8 w-8"
+				>
+					{expandedRows.has(row.original.id) ? (
+						<ChevronDownIcon className="h-4 w-4" />
+					) : (
+						<ChevronRightIcon className="h-4 w-4" />
+					)}
+				</Button>
+			),
+			enableSorting: false,
+			enableHiding: false,
+			size: 10,
 		},
-	},
-];
+		{
+			accessorKey: "orderNumber",
+			header: "Order Number",
+		},
+		{
+			accessorKey: "username",
+			header: "Username",
+		},
+		{
+			accessorKey: "createdAt",
+			header: "Created At",
+			cell(props) {
+				const date = new Date(props.getValue() as string);
+				return (
+					<span>
+						{date.toLocaleDateString("en-US", {
+							year: "numeric",
+							month: "short",
+							day: "2-digit",
+							hour: "2-digit",
+							minute: "2-digit",
+						})}
+					</span>
+				);
+			},
+		},
+	];
 
-interface DataTableProps<TData, TValue> {
-	columns: ColumnDef<TData, TValue>[];
-	data: TData[];
-}
-
-export function DataTable<TData, TValue>({
-	columns,
-	data,
-}: DataTableProps<TData, TValue>) {
 	const table = useReactTable({
 		data,
 		columns,
@@ -73,69 +143,196 @@ export function DataTable<TData, TValue>({
 		getPaginationRowModel: getPaginationRowModel(),
 	});
 
-	const { openDialog } = useDialogStore();
-
 	return (
-		<div className="rounded-md border">
-			<div className="flex justify-between items-center px-2 py-4">
-				<SearchFilter />
-
-				<Button
-					variant="outline"
-					onClick={() => {
-						openDialog({
-							children: <AddOrderDialogContainer />,
-						});
-					}}
-				>
-					Add Order
-				</Button>
-			</div>
-			<Table>
-				<TableHeader>
-					{table.getHeaderGroups().map((headerGroup) => (
-						<TableRow key={headerGroup.id}>
-							{headerGroup.headers.map((header) => {
-								return (
-									<TableHead key={header.id}>
-										{header.isPlaceholder
-											? null
-											: flexRender(
-													header.column.columnDef.header,
-													header.getContext(),
-												)}
-									</TableHead>
-								);
-							})}
-						</TableRow>
-					))}
-				</TableHeader>
-				<TableBody>
-					{table.getRowModel().rows?.length ? (
-						table.getRowModel().rows.map((row) => (
+		<Table>
+			<TableHeader>
+				{table.getHeaderGroups().map((headerGroup) => (
+					<TableRow key={headerGroup.id} className="border-b border-gray-200">
+						{headerGroup.headers.map((header) => {
+							return (
+								<TableHead
+									key={header.id}
+									className="font-semibold text-gray-900"
+									style={{
+										width: header.column.columnDef.size || "auto",
+										minWidth: header.column.columnDef.size || "auto",
+										maxWidth: header.column.columnDef.size || "auto",
+									}}
+								>
+									{header.isPlaceholder
+										? null
+										: flexRender(
+												header.column.columnDef.header,
+												header.getContext(),
+											)}
+								</TableHead>
+							);
+						})}
+					</TableRow>
+				))}
+			</TableHeader>
+			<TableBody>
+				{table.getRowModel().rows?.length ? (
+					table.getRowModel().rows.map((row) => (
+						<Fragment key={row.id}>
 							<TableRow
-								key={row.id}
 								data-state={row.getIsSelected() && "selected"}
+								className="border-b border-gray-100 hover:bg-gray-50"
 							>
 								{row.getVisibleCells().map((cell) => (
-									<TableCell key={cell.id}>
+									<TableCell
+										key={cell.id}
+										className="py-4"
+										style={{
+											width: cell.column.columnDef.size || "auto",
+											minWidth: cell.column.columnDef.size || "auto",
+											maxWidth: cell.column.columnDef.size || "auto",
+										}}
+									>
 										{flexRender(cell.column.columnDef.cell, cell.getContext())}
 									</TableCell>
 								))}
 							</TableRow>
-						))
-					) : (
-						<TableRow>
-							<TableCell colSpan={columns.length} className="h-24 text-center">
-								No results.
-							</TableCell>
-						</TableRow>
-					)}
-				</TableBody>
-			</Table>
-			<div className="p-2 pt-3 border-t">
-				<DataTablePagination table={table} />
-			</div>
-		</div>
+							{expandedRows.has(row.original.id) && (
+								<TableRow className="bg-gray-50">
+									<TableCell colSpan={columns.length} className="p-0">
+										<div className="px-6 py-4 space-y-2">
+											<div className="flex flex-row items-center justify-between">
+												<h4 className="font-medium text-gray-900 mb-3">
+													Products
+												</h4>
+											</div>
+											<div className="space-y-2">
+												{row.original.products.length === 0 && (
+													<p className="text-gray-500">No products.</p>
+												)}
+												{row.original.products.map((variant, idx) => {
+													const downloadImage = async () => {
+														const { imageUrl } = variant;
+														if (!imageUrl) {
+															toast.error(
+																"No image URL available for download.",
+															);
+															return;
+														}
+
+														const key = imageUrl.split("/").pop();
+
+														const { data: response, error: fetchErr } =
+															await tryCatch(fetch(`/api/files/${key}`));
+														if (fetchErr) {
+															toast.error("Failed to fetch the image URL.", {
+																description: fetchErr.message,
+															});
+															return;
+														}
+
+														if (!response.ok) {
+															toast.error("Failed to download the image.");
+															return;
+														}
+
+														const { data: blob, error: blobErr } =
+															await tryCatch(response.blob());
+														if (blobErr) {
+															toast.error(
+																"Failed to convert response to blob.",
+															);
+															return;
+														}
+
+														const url = URL.createObjectURL(blob);
+														const a = document.createElement("a");
+														a.href = url;
+														a.download =
+															imageUrl.split("/").pop() || "image.png";
+														document.body.appendChild(a);
+														a.click();
+														document.body.removeChild(a);
+														URL.revokeObjectURL(url);
+													};
+
+													return (
+														<div
+															key={`${variant.id}-${idx}`}
+															className="flex items-center justify-between p-2 border border-gray-200 rounded-md bg-background"
+														>
+															<div className="flex items-center space-x-4">
+																{variant.imageUrl ? (
+																	<img
+																		src={variant.imageUrl}
+																		alt={variant.name}
+																		className="w-16 h-16 object-cover rounded-md border"
+																	/>
+																) : (
+																	<div className="w-16 h-16 bg-gray-200 rounded-md flex items-center justify-center">
+																		<span className="text-gray-500 text-xs">
+																			No Image
+																		</span>
+																	</div>
+																)}
+																<div>
+																	<p className="font-medium text-gray-900">
+																		{variant.name}
+																	</p>
+																	<p className="text-sm text-gray-500">
+																		{variant.productVariant.name} (
+																		{variant.productVariant.width}x
+																		{variant.productVariant.height})
+																	</p>
+																</div>
+															</div>
+															{variant.imageUrl ? (
+																<Dialog>
+																	<DialogTrigger asChild>
+																		<Button variant="outline" size="icon">
+																			<EyeIcon />
+																		</Button>
+																	</DialogTrigger>
+																	<DialogContent>
+																		<DialogHeader>
+																			<DialogTitle>Image Preview</DialogTitle>
+																			<DialogDescription>
+																				View the image associated with this
+																				order.
+																			</DialogDescription>
+																		</DialogHeader>
+																		<Image
+																			src={variant.imageUrl}
+																			alt="Order Image"
+																			width={500}
+																			height={500}
+																			className="object-cover border"
+																		/>
+																		<DialogFooter>
+																			<Button
+																				onClick={downloadImage}
+																				variant="outline"
+																			>
+																				Download Image
+																			</Button>
+																		</DialogFooter>
+																	</DialogContent>
+																</Dialog>
+															) : null}
+														</div>
+													);
+												})}
+											</div>
+										</div>
+									</TableCell>
+								</TableRow>
+							)}
+						</Fragment>
+					))
+				) : (
+					<TableRow>
+						<TableCell colSpan={columns.length} className="h-24 text-center">
+							No results.
+						</TableCell>
+					</TableRow>
+				)}
+			</TableBody>
+		</Table>
 	);
 }
