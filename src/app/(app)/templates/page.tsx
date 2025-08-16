e90fc76aee75f39805ee2b5301e93ca8171ebf02
ahargunyllib <billy.bpm03@gmail.com>
2025-08-16 22:35:51 +0700

    feat: add confirmation dialog for template submission with success feedback

--- src/app/(app)/templates/page.tsx
+++ src/app/(app)/templates/page.tsx
@@ -8,7 +8,15 @@ import {
 	AccordionTrigger,
 } from "@/shared/components/ui/accordion";
 import { Button } from "@/shared/components/ui/button";
+import {
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+} from "@/shared/components/ui/dialog";
 import { Tabs, TabsList, TabsTrigger } from "@/shared/components/ui/tabs";
+import { useDialogStore } from "@/shared/hooks/use-dialog";
 import { useIsMobile } from "@/shared/hooks/use-mobile";
 import { getTemplateForSize } from "@/shared/lib/template";
 import { cn } from "@/shared/lib/utils";
@@ -51,6 +59,42 @@ export default function Page() {
 
 	const { mutate: submitOrder, isPending } = useSubmitOrderMutation();
 	const router = useRouter();
+	const { openDialog, closeDialog } = useDialogStore();
+
+	const onSubmitHandler = () => {
+		const templates = [];
+		for (const productVariant of productVariants) {
+			for (const template of productVariant.templates) {
+				if (template.dataURL) {
+					templates.push({
+						orderProductVariantId: template.id,
+						dataURL: template.dataURL,
+					});
+				}
+			}
+		}
+		submitOrder(
+			{
+				orderId: id,
+				templates,
+			},
+			{
+				onSuccess: (res) => {
+					closeDialog();
+					if (!res.success) {
+						toast.error("Gagal mengirim template, silakan coba lagi.", {
+							description: res.message || "Terjadi kesalahan",
+						});
+						return;
+					}
+
+					toast.success("Template berhasil dikirim!");
+					deleteOrder();
+					router.replace("/templates/onboarding");
+				},
+			},
+		);
+	};
 
 	return (
 		<section className="relative">
@@ -135,54 +179,60 @@ export default function Page() {
 
 							{hasFillAllTemplates && (
 								<Button
-									onClick={() => {
-										const templates = [];
-										for (const productVariant of productVariants) {
-											for (const template of productVariant.templates) {
-												if (template.dataURL) {
-													templates.push({
-														orderProductVariantId: template.id,
-														dataURL: template.dataURL,
-													});
-												}
-											}
-										}
-										submitOrder(
-											{
-												orderId: id,
-												templates,
-											},
-											{
-												onSuccess: (res) => {
-													if (!res.success) {
-														toast.error(
-															"Gagal mengirim template, silakan coba lagi.",
-															{
-																description: res.message || "Terjadi kesalahan",
-															},
-														);
-														return;
-													}
-
-													toast.success("Template berhasil dikirim!");
-													deleteOrder();
-													router.replace("/templates/onboarding");
-												},
-											},
-										);
-									}}
 									className="gap-2 font-medium text-xs text-white bg-black px-4 py-3 rounded-md h-fit"
+									onClick={() => {
+										openDialog({
+											children: (
+												<DialogContent
+													showCloseButton={false}
+													className="sm:max-w-sm"
+												>
+													<div className="relative size-48 w-full">
+														<Image
+															src="/svgs/success-1.svg"
+															alt="Success"
+															fill
+															objectFit="contain"
+														/>
+													</div>
+													<DialogHeader className="gap-4 sm:text-center">
+														<DialogTitle className="text-center text-[#1D2939] font-bold">
+															Konfirmasi template pemesanan
+														</DialogTitle>
+														<DialogDescription className="text-center text-[#737373] text-sm">
+															Apakah kamu yakin templatenya sudah cocok? kalo
+															udah oke bisa kirim ke kami dan tunggu pesanan
+															kamu sampe yaa
+														</DialogDescription>
+													</DialogHeader>
+													<DialogFooter className="flex flex-row gap-2">
+														<Button
+															variant="secondary"
+															className="flex-1 px-8 py-4 h-fit bg-[#F2F4F7] hover:bg-[#dcdcdf] rounded-md shadow-none text-base font-bold text-[#344054]"
+															onClick={() => closeDialog()}
+														>
+															Kembali
+														</Button>
+														<Button
+															onClick={() => onSubmitHandler()}
+															className="flex-1 px-8 py-4 h-fit bg-[#2854AD] hover:bg-[#2854AD]/80 rounded-md shadow-none text-base font-bold text-[#ffffff]"
+														>
+															{isPending ? (
+																<>
+																	<LoaderIcon className="animate-spin" />
+																	Mengirim...
+																</>
+															) : (
+																<>Kirim</>
+															)}
+														</Button>
+													</DialogFooter>
+												</DialogContent>
+											),
+										});
+									}}
 								>
-									{isPending ? (
-										<>
-											<LoaderIcon className="animate-spin" />
-											Mengirim...
-										</>
-									) : (
-										<>
-											Kirim Template <SendIcon />
-										</>
-									)}
+									Kirim Template <SendIcon />
 								</Button>
 							)}
 						</div>
@@ -202,9 +252,10 @@ export default function Page() {
 											className="size-40 object-contain rounded-lg border"
 										/>
 										<Button
-											variant="destructive"
+											variant="ghost"
 											size="sm"
 											onClick={() => deleteDataURLTemplate(template.id)}
+											className="text-destructive hover:text-destructive font-semibold hover:font-bold w-full"
 										>
 											Hapus
 										</Button>
