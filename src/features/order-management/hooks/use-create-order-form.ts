c7a2acd1dc0968e344b1c5c8b387da277d75e5dd
ahargunyllib <billy.bpm03@gmail.com>
2025-08-17 16:41:02 +0700

    feat: add useFieldArray to useCreateOrderForm for enhanced form handling

--- src/features/order-management/hooks/use-create-order-form.ts
+++ src/features/order-management/hooks/use-create-order-form.ts
@@ -7,7 +7,7 @@ import {
 } from "@/shared/repository/order/dto";
 import { useCreateOrderMutation } from "@/shared/repository/order/query";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 import { toast } from "sonner";
 
 export const useCreateOrderForm = () => {
