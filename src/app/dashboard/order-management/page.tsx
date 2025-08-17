7d19bf99fb0e6358e39f162396fb4c0a41a3ef4f
ahargunyllib <billy.bpm03@gmail.com>
2025-08-17 19:13:22 +0700

    feat: refactor order management page to use OrdersTableContainer and improve loading handling

--- src/app/dashboard/order-management/page.tsx
+++ src/app/dashboard/order-management/page.tsx
@@ -1,8 +1,4 @@
-"use client";
-
-import OrderTable from "@/features/order-management/components/order-table";
-import AddOrderDialogContainer from "@/features/order-management/container/add-order-dialog-container";
-import { Button } from "@/shared/components/ui/button";
+import OrdersTableContainer from "@/features/order-management/container/orders-table-container";
 import {
 	Card,
 	CardContent,
@@ -10,10 +6,7 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/shared/components/ui/card";
-import { useDialogStore } from "@/shared/hooks/use-dialog";
-import { useGetOrdersQuery } from "@/shared/repository/order/query";
-import { useSearchParams } from "next/navigation";
-import { Fragment, Suspense } from "react";
+import { Suspense } from "react";
 
 export default function Page() {
 	return (
@@ -27,45 +20,9 @@ export default function Page() {
 			</CardHeader>
 			<CardContent>
 				<Suspense>
-					<OrderContainer />
+					<OrdersTableContainer />
 				</Suspense>
 			</CardContent>
 		</Card>
-	);
-}
-
-function OrderContainer() {
-	const sp = useSearchParams();
-
-	const { data: res, isLoading } = useGetOrdersQuery({
-		search: sp.get("search") || "",
-		page: Number(sp.get("page")) || 1,
-		limit: Number(sp.get("limit")) || 10,
-	});
-
-	const { openDialog } = useDialogStore();
-
-	if (isLoading) return <div>Loading...</div>;
-
-	if (!res?.data) return <div>No data</div>;
-
-	const orders = res.data.orders;
-
-	return (
-		<Fragment>
-			<div className="flex justify-between items-center px-2 py-4">
-				<Button
-					variant="outline"
-					onClick={() => {
-						openDialog({
-							children: <AddOrderDialogContainer />,
-						});
-					}}
-				>
-					Add Order
-				</Button>
-			</div>
-			<OrderTable data={orders} />
-		</Fragment>
 	);
 }
