d5a2df070321ff4deb8e8438b79befe3f45c7dac
ahargunyllib <billy.bpm03@gmail.com>
2025-10-01 10:18:35 +0700

    feat: wrap Page component in Suspense for improved loading handling

--- src/app/(app)/old/page.tsx
+++ src/app/(app)/old/page.tsx
@@ -1,11 +1,12 @@
 import OnboardingFormModalContainer from "@/features/editor/containers/onboarding-form-modal-container";
 import TemplateSelector from "@/features/editor/containers/template-selector";
+import { Suspense } from "react";
 
 export default function Page() {
 	return (
-		<>
+		<Suspense>
 			<TemplateSelector />
 			<OnboardingFormModalContainer />
-		</>
+		</Suspense>
 	);
 }
