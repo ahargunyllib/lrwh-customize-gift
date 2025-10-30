0c1702468ae1d1140f5d014f6879a5554153f99b
ahargunyllib <billy.bpm03@gmail.com>
2025-10-30 07:27:35 +0700

    feat: Remove unused SVG files and update onboarding page with new vector graphics for improved design consistency.

--- src/features/templates/components/background.tsx
+++ src/features/templates/components/background.tsx
@@ -2,29 +2,47 @@ import Image from "next/image";
 
 export default function Background() {
 	return (
-		<div className="absolute top-0 left-0 inset-0 min-h-dvh bg-[#F2F4F7] w-screen z-[-1] overflow-hidden">
-			<div className="absolute size-28 md:size-30 left-[18.5rem] top-[10.5rem]">
+		<div className="absolute top-0 left-0 inset-0 min-h-dvh bg-[#F2EFE8] w-screen z-[-1] overflow-hidden">
+			<div className="absolute size-[8rem] right-[25%] bottom-[-2rem]">
 				<Image
-					src="/svgs/peach-flower.svg"
-					alt="Peach Flower"
+					src="/svgs/vector-6.svg"
+					alt="Vector 6"
 					fill
 					className="object-contain"
 				/>
 			</div>
 
-			<div className="absolute size-36 left-[8rem] bottom-[-2rem] md:visible invisible">
+			<div className="absolute size-[8rem] left-[20%] top-[15%]">
 				<Image
-					src="/svgs/pink-floop.svg"
-					alt="Pink Floop"
+					src="/svgs/vector-7.svg"
+					alt="Vector 7"
 					fill
 					className="object-contain"
 				/>
 			</div>
 
-			<div className="absolute size-48 right-[-1rem] top-[2.5rem] md:visible invisible">
+			<div className="absolute size-[11rem] right-[-2rem] top-[2rem]">
 				<Image
-					src="/svgs/light-yellow-quarter-outline-circle.svg"
-					alt="Light Yellow Quarter Outline Circle"
+					src="/svgs/vector-8.svg"
+					alt="Vector 8"
+					fill
+					className="object-contain"
+				/>
+			</div>
+
+			<div className="absolute size-[7rem] right-[3rem] top-[30%]">
+				<Image
+					src="/svgs/vector-9.svg"
+					alt="Vector 9"
+					fill
+					className="object-contain"
+				/>
+			</div>
+
+			<div className="absolute size-[9rem] left-[-2rem] bottom-[2rem]">
+				<Image
+					src="/svgs/vector-10.svg"
+					alt="Vector 10"
 					fill
 					className="object-contain"
 				/>
