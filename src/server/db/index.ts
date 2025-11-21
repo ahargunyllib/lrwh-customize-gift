b029444aa6c92da3c4144b028843eba1a8ffa3b0
ahargunyllib <billy.bpm03@gmail.com>
2025-11-21 12:08:09 +0700

    fix(db): replace @neondatabase/serverless with pg for database connection

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,8 +1,9 @@
-import { Pool } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-serverless";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 import { env } from "../../env.mjs";
 
 const pool = new Pool({
 	connectionString: env.DATABASE_URL,
 });
+
 export const db = drizzle({ client: pool });
